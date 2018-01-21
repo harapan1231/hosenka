e15be199e29b3715342e896bf419bda20431efbe
Linux User <nabbi24@ip-172-31-11-3.us-west-1.compute.internal>
2018-01-21 01:30:18 +0000

    Add a test model

--- search/views.py
+++ search/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Ret
 
 def index(request):
     template = loader.get_template('search/index.html')
     context = {
         'title': 'Search',
+        'ret': Ret.objects.all(),
     }
     return HttpResponse(template.render(context, request))
 
