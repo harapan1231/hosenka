886493b1351e04b2baa4e26b0d723e3ea870791b
nabbi24 <nabbi24@protonmail.com>
2018-01-23 20:48:29 +0900

    Add a model: Totaling

--- fees/models.py
+++ fees/models.py
@@ -6,3 +6,8 @@ class SearchResult(models.Model):
     category = models.CharField(max_length=30)
     published = models.DateField()
 
+class Totaling(models.Model):
+    ins_id = models.CharField(max_length=12)
+    history = models.IntegerField
+    amount_fee = models.DecimalField(max_digits=8, decimal_places=0)
+
